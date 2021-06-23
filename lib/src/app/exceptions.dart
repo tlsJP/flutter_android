061dbb27c339dd73d188e8c8cd617d8cd5b684cc
JP <jpalbert81@gmail.com>
2021-06-23 10:32:19 -0700

    add no-arg constructor

--- lib/src/app/exceptions.dart
+++ lib/src/app/exceptions.dart
@@ -16,6 +16,8 @@ import '../os/parcelable.dart' show Parcelable;
 /// See: https://github.com/aosp-mirror/platform_frameworks_base/blob/master/core/java/android/app/AuthenticationRequiredException.java
 class AuthenticationRequiredException extends PlatformException
     with Parcelable {
+  AuthenticationRequiredException() : super(code: 'AUTHENTICATION_REQUIRED');
+
   @override
   String get parcelableCreator => "android.app.AuthenticationRequiredException";
 
